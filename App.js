2f8e5e1f4e089046ea138166565e8a6f4169ad64
vignagajan <my.soclntwrks@gmail.com>
2021-04-23 17:06:29 +0530

    Add Poppins fonts

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+let customFonts = {
+  'Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+  'Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+  'Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+  'SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+};
+
+export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (this.state.fontsLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Platform Default</Text>
+          <Text style={{ fontFamily: 'Regular' }}>Regular</Text>
+          <Text style={{ fontFamily: 'Bold' }}>Bold</Text>
+          <Text style={{ fontFamily: 'Medium' }}>Medium</Text>
+          <Text style={{ fontFamily: 'SemiBold' }}>SemiBold</Text>
+          
+        </View>
+      );
+    } else {
+      return <AppLoading />;
+    }
+  }
 }
 
 const styles = StyleSheet.create({
